79ef447b3a201d22eb7148eebe16c2bc56efa759
AppleSeed <everlastingfriendship194@gmail.com>
2024-03-28 10:33:56 +0900

    fix:refactor server structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Serve static files from the public directory
 console.log(__dirname);
